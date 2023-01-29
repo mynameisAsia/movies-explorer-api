aa1cea6c52870ccd15458c3be71fec8c6d553e7e
Asia Mamatova <a.mamatova9991@gmail.com>
2023-01-29 11:44:58 +0300

    added helmet, cors, ratelimit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const router = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -10,12 +13,22 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(requestLogger);
+app.use(helmet());
+app.use(limiter);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
